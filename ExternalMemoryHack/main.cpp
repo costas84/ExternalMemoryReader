2f5a6e17894b2febe5aec906ee63d775d423948c
Alpha <Alpha@DESKTOP-SIBM4J9>
2023-05-28 19:17:41 +0300

    The API to write to another process's memory is called WriteProcessMemory. Its definition is very similar to ReadProcessMemory:

--- ExternalMemoryHack/main.cpp
+++ ExternalMemoryHack/main.cpp
@@ -10,6 +10,14 @@ int main(int argc, char** argv) {
 
 	DWORD gold_value = 0;
 	DWORD bytes_read = 0;
-	ReadProcessMemory(wesnoth_process, (void*)0x017EECB8, &gold_value, 4, &bytes_read);
+	ReadProcessMemory(wesnoth_process, (void*)0x017EED18, &gold_value, 4, &bytes_read);
+
+	gold_value += 0xA90;
+	ReadProcessMemory(wesnoth_process, (void*)gold_value, &gold_value, 4, &bytes_read);
+	gold_value += 4;
+	DWORD new_gold_value = 555;
+	DWORD bytes_written = 0;
+	WriteProcessMemory(wesnoth_process, (void*)gold_value, &new_gold_value, 4, &bytes_written);
+
 	return 0;
 }
